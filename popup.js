c119ea5c3743fa5ba3149d1ae3668d67b9fcfb3e
kaltera <kaltera@gmail.com>
2015-10-15 11:26:22 +0100

    V1.1
    Added gartner.com/document filter to take only g.com
    Added text/image on click of the button

--- popup.js
+++ popup.js
@@ -20,31 +20,31 @@ var queryInfo = {
 
 var gartner_ids = "";
 
-  chrome.tabs.query(queryInfo, function(tabs) {
-    // chrome.tabs.query invokes the callback with a list of tabs that match the
-    // query. When the popup is opened, there is certainly a window and at least
-    // one tab, so we can safely assume that |tabs| is a non-empty array.
-    // A window can only have one active tab at a time, so the array consists of
-    // exactly one tab.
-    // A tab is a plain object that provides information about the tab.
-    // See https://developer.chrome.com/extensions/tabs#type-Tab
-    for (tab in tabs){
-      var url = tabs[tab].url;
-      var pattern = /([document\v/]|[research\v/]){9}[0-9]{5,10}/g;
-      if(url.indexOf("gartner") != -1){
-        var url_match = url.match(pattern)[0];
-        if(url_match != ""){
-          var doc_type = url_match.split("/");
-          if(gartner_ids == ""){
-            gartner_ids = doc_type[1];
-          }
-          else{
-            if(gartner_ids.indexOf(doc_type[1]) == -1){
-              gartner_ids += " OR " + doc_type[1];
-            }
+chrome.tabs.query(queryInfo, function(tabs) {
+  // chrome.tabs.query invokes the callback with a list of tabs that match the
+  // query. When the popup is opened, there is certainly a window and at least
+  // one tab, so we can safely assume that |tabs| is a non-empty array.
+  // A window can only have one active tab at a time, so the array consists of
+  // exactly one tab.
+  // A tab is a plain object that provides information about the tab.
+  // See https://developer.chrome.com/extensions/tabs#type-Tab
+  for (tab in tabs){
+    var url = tabs[tab].url;
+    var pattern = /([document\v/]|[research\v/]){9}[0-9]{5,10}/g;
+    if(url.indexOf("gartner.com/document") != -1){
+      var url_match = url.match(pattern)[0];
+      if(url_match != ""){
+        var doc_type = url_match.split("/");
+        if(gartner_ids == ""){
+          gartner_ids = doc_type[1];
+        }
+        else{
+          if(gartner_ids.indexOf(doc_type[1]) == -1){
+            gartner_ids += " OR " + doc_type[1];
           }
         }
       }
-    copyToClipboard(gartner_ids);
+    }
+  copyToClipboard(gartner_ids);
   }
 });
