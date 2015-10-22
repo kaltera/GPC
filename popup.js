b7c4e8421bedcf035681f33f47a05120813d1a4f
kaltera <kaltera@gmail.com>
2015-10-22 08:55:17 +0100

    V1.7
    Added Google Analytics

--- popup.js
+++ popup.js
@@ -2,6 +2,16 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// Google Analytics
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-69105271-1', 'auto');
+  ga('set', 'checkProtocolTask', null);
+  ga('send', 'pageview', location.pathname);
+
 // Function to copy text to clipboard
 function copyToClipboard( text ){
     var copyDiv = document.createElement('div');
